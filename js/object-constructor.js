86e0c370b738574b7b0ce3ae6807d5882ba22e3b
NguyenHong2022 <106530389+NguyenHong2022@users.noreply.github.com>
2022-07-27 21:10:04 +0700

    javascrip03

---
+++ js/object-constructor.js
@@ -0,0 +1,14 @@
+var hotel = new Object();
+
+hotel.name = 'Park';
+hotel.rooms = 120;
+hotel.booked = 77;
+hotel.checkAvailability = function () {
+    return this.rooms - this.booked;
+};
+
+var elName = document.getElementById('hotelName');
+elName.textContent = hotel.name;
+
+var elName = document.getElementById('rooms');
+elRooms.textContent = hotel.checkAvailability();
